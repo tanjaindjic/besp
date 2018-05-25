b8214005ae626e31244ec2a06d83c83fe48d2e68
Nina <ninamiladinovic@hotmail.com>
2018-05-25 16:01:41 +0200

    sredjivan html za sertifikate

--- projekat/src/main/resources/static/app/components/certificate/certificate.controller.js
+++ projekat/src/main/resources/static/app/components/certificate/certificate.controller.js
@@ -13,19 +13,26 @@
         	$scope.status = "";
         	$scope.showStatus= false;
         	$scope.cert = {};
+        	$scope.isAll=true;
+        	$scope.isCheck=false;
+        	$scope.isMake=false;
+        	$scope.isGet=false;
+        	$scope.isRevoke=false;
+        	$scope.checkedStatus="";
+        	$scope.showCheckedStatus=false;
+        	$scope.showIsRevoked=false;
+        	$scope.revokedStatus="";
+        	$scope.allCerts = [];
+        	
      
-        /*	$http({
+        	$http({
                 method: 'GET',
-                url: 'http://localhost:8096/user/'
-
+                url: 'http://localhost:8096/certificates'
               }).then(function successCallback(response) {
-            	  alert(response.data)
-                  $scope.users = response.data;
-            	  alert($scope.users.length)
+            	  for(var i=0; i<response.data.length; i++)            		  
+            		  $scope.allCerts.push(response.data[i]);
               });    
               
-        	if($cookies.get('user'))
-                $location.path("home")*/
         }
         
         init();
@@ -35,24 +42,74 @@
         	$scope.cert.startDate=$scope.dateFrom;
         	$scope.cert.endDate=$scope.dateTo;
         	$scope.cert.caa=1;
+        	$scope.cert.serialNumber=0;
         	var data = $scope.cert;
         	$http({
                 method: 'POST',
                 url: 'http://localhost:8096/certificates',
                 data: data
               }).then(function successCallback(response) {
-            	  $scope.status="Certificate made succesfully!";
+            	  $scope.status="Certificate made succesfully! SERIAL NUMBER:" + response.data.serialNumber;
             	  crc.showDone();
               });  
         	
         	
         }
-       
+        crc.all = function(){
+        	$scope.isAll=true;
+        	$scope.isCheck=false;
+        	$scope.isMake=false;
+        	$scope.isGet=false;
+        	$scope.isRevoke=false;
+        	
+        }
+        crc.makeNew = function(){
+        	$scope.isAll=false;
+        	$scope.isCheck=false;
+        	$scope.isMake=true;
+        	$scope.isGet=false;
+        	$scope.isRevoke=false;
+        	
+        }
+        crc.getCert = function(){
+        	$scope.isAll=false;
+        	$scope.isCheck=false;
+        	$scope.isMake=false;
+        	$scope.isGet=true;
+        	$scope.isRevoke=false;
+        	
+        }
+        crc.check = function(){
+        	$scope.isAll=false;
+        	$scope.isCheck=true;
+        	$scope.isMake=false;
+        	$scope.isGet=false;
+        	$scope.isRevoke=false;        	
+        }
+        crc.revokeCert = function(){
+        	$scope.isAll=false;
+        	$scope.isCheck=false;
+        	$scope.isMake=false;
+        	$scope.isGet=false;
+        	$scope.isRevoke=true;
+        }
+        crc.logOut = function(){
+        	
+        }
+        crc.download = function(){
+        	
+        }
+        crc.checkStatus = function(){
+        	
+        }
+        crc.revokeCertificate = function(){
+        	
+        }
         crc.showDone= function() {			
 		      $scope.showStatus= true;
 		      $timeout(function() {
 		         $scope.showStatus = false;
-		      }, 3000);
+		      }, 20000);
 		   };
     }
 
